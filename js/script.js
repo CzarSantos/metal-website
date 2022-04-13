3dca84300ed52a5bdea3ecc73688820b8aa16b68
Julio Cesar <czar.santos@outlook.com>
2022-04-13 12:26:37 -0300

    Ajustes e efeitos

--- js/script.js
+++ js/script.js
@@ -30,3 +30,46 @@ new SimpleSlide({
   time: 5000,
   nav:true
 });
+
+
+
+
+/* EVENTO SCROLL --------------------------------------- */
+
+const item = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () => {
+
+  //top da pagina
+  const windowTop = window.pageYOffset + window.innerHeight * 0.75
+  
+  item.forEach(element => {
+    //se distancia da tela para o topo for maior que a distancia do meu elemento para o topo
+    if(windowTop > element.offsetTop){
+      element.classList.add('animate')
+    }else{
+      element.classList.remove('animate')
+    }
+  })
+
+  
+
+
+}
+
+//execução
+animeScroll()
+
+window.addEventListener('scroll', ()=>{
+  animeScroll()
+})
+
+
+
+//////////////////////////////
+
+/* Rellax */
+
+const rallax = new Rellax('.rellax');
+
+
